3dd31caab7b3cf14729377816545880516e31163
AlexReedy <alexander.h.reedy@gmail.com>
2021-11-15 13:54:13 -0800

    Cleaning

--- dynamic_fitting_plot_all_test.py
+++ dynamic_fitting_plot_all_test.py
@@ -5,7 +5,7 @@ fit = FittingLibrary(pause=.5)
 start = timeit.default_timer()
 counter = 0
 for data_set in fit.data_sets:
-    print(f'Completed: {counter} of {len(fit.data_sets)}')
+    ind_start = timeit.default_timer()
     fit.import_data(file=data_set)
     #fit.plot_mag(save=False, show=True)
     #fit.plot_flux(save=False, show=True)
@@ -15,6 +15,10 @@ for data_set in fit.data_sets:
     fit.plot_flux_dynamic()
     fit.sigma_clipping()
     fit.plot_sigma_clip_dynamic()
+    ind_end = timeit.default_timer() - ind_start
+    print(f'{data_set} [{counter} / {len(fit.data_sets)}] Completed:\n'
+          f'Runtime: {int(ind_end)} sec\n'
+          f'Moving to Next Data Set!')
     counter += 1
 runtime = timeit.default_timer() - start
-print(f'Runtime: {int(runtime)} seconds')
+print(f'Total Runtime: {int(runtime)} sec')
